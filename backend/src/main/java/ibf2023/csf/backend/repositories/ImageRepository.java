25498c9578453e2ee92b89a29c53d5a6a368891a
sescc <codingscc@gmail.com>
2024-06-14 16:40:46 +0800

    Mongo & S3 uploads

--- backend/src/main/java/ibf2023/csf/backend/repositories/ImageRepository.java
+++ backend/src/main/java/ibf2023/csf/backend/repositories/ImageRepository.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
+import ibf2023.csf.backend.models.Image;
+
 @Repository
 public class ImageRepository {
 
@@ -13,8 +15,9 @@ public class ImageRepository {
 	// TODO Task 4.1
 	// You may change the method signature by adding parameters and/or the return type
 	// You may throw any exception 
-	public void save() {
+	public void save(Image img) {
 		// IMPORTANT: Write the native mongo query in the comments above this method
+		mongoTemplate.save(img, "travelpics");
 	}
 }
 
--- backend/src/main/java/ibf2023/csf/backend/repositories/PictureRepository.java
+++ backend/src/main/java/ibf2023/csf/backend/repositories/PictureRepository.java
@@ -1,9 +1,19 @@
 package ibf2023.csf.backend.repositories;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Repository
 public class PictureRepository {
@@ -14,7 +24,27 @@ public class PictureRepository {
 	// TODO Task 4.2
 	// You may change the method signature by adding parameters and/or the  return type
 	// You may throw any exception 
-	public void save() {
+	public String save(MultipartFile file) throws IOException {
+		final String bucketname = "csf";
+		Map<String, String> userData = new HashMap<>();
+
+		userData.put("dateUploaded", (new Date()).toString());
+		userData.put("filename", file.getOriginalFilename());
+
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setContentLength(file.getSize());
+		metadata.setContentType(file.getContentType());
+		metadata.setUserMetadata(userData);
+
+		String key = UUID.randomUUID().toString().substring(0,8);
+		
+		PutObjectRequest putReq = new PutObjectRequest(bucketname, key, file.getInputStream(),metadata);
+		putReq = putReq.withCannedAcl(CannedAccessControlList.PublicRead);
+
+		s3.putObject(putReq);
+
+		return s3.getUrl(bucketname, key).toString();
+
 	}
 
 }
