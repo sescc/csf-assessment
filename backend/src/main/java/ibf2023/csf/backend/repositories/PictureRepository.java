df358380e911547e94d0b11ab3dd8ee9855b6cdc
sescc <codingscc@gmail.com>
2024-06-14 16:42:31 +0800

    Mongo & S3 func swap

--- backend/src/main/java/ibf2023/csf/backend/repositories/PictureRepository.java
+++ backend/src/main/java/ibf2023/csf/backend/repositories/PictureRepository.java
@@ -1,50 +1,23 @@
 package ibf2023.csf.backend.repositories;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import ibf2023.csf.backend.models.Image;
 
 @Repository
 public class PictureRepository {
 
 	@Autowired
-	private AmazonS3 s3;
+	private MongoTemplate mongoTemplate;
 
-	// TODO Task 4.2
-	// You may change the method signature by adding parameters and/or the  return type
+	// TODO Task 4.1
+	// You may change the method signature by adding parameters and/or the return type
 	// You may throw any exception 
-	public String save(MultipartFile file) throws IOException {
-		final String bucketname = "csf";
-		Map<String, String> userData = new HashMap<>();
-
-		userData.put("dateUploaded", (new Date()).toString());
-		userData.put("filename", file.getOriginalFilename());
-
-		ObjectMetadata metadata = new ObjectMetadata();
-		metadata.setContentLength(file.getSize());
-		metadata.setContentType(file.getContentType());
-		metadata.setUserMetadata(userData);
-
-		String key = UUID.randomUUID().toString().substring(0,8);
-		
-		PutObjectRequest putReq = new PutObjectRequest(bucketname, key, file.getInputStream(),metadata);
-		putReq = putReq.withCannedAcl(CannedAccessControlList.PublicRead);
-
-		s3.putObject(putReq);
-
-		return s3.getUrl(bucketname, key).toString();
-
+	public void save(Image img) {
+		// IMPORTANT: Write the native mongo query in the comments above this method
+		mongoTemplate.save(img, "travelpics");
 	}
 
 }
